7fb15aa1fd632582c7bfdfff54aef33c345e035d
nadiand <nadetodobreva@gmail.com>
2023-12-11 12:22:51 +0100

    Update test file

--- test.py
+++ test.py
@@ -3,36 +3,32 @@ from model import TransformerClassifier
 from training import predict
 from dataset import HitsDataset, get_dataloaders, load_linear_3d_data, load_linear_2d_data, load_curved_3d_data
 from plotting import *
+from trackml_data import load_trackml_data
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-transformer = TransformerClassifier(num_encoder_layers=8,
+transformer = TransformerClassifier(num_encoder_layers=6,
                                     d_model=32,
-                                    n_head=16,
+                                    n_head=8,
                                     input_size=3,
-                                    output_size=2,
-                                    dim_feedforward=256,
+                                    output_size=3,
+                                    dim_feedforward=128,
                                     dropout=0.1)
 transformer = transformer.to(DEVICE)
-checkpoint = torch.load("3d_3curved_100epoch_big_best", map_location=torch.device('cpu'))
+checkpoint = torch.load("trackml_1to50tracks_3out_last", map_location=torch.device('cpu'))
 transformer.load_state_dict(checkpoint['model_state_dict'])
 pytorch_total_params = sum(p.numel() for p in transformer.parameters() if p.requires_grad)
 print("Total trainable params: {}".format(pytorch_total_params))
 
-hits_data, track_params_data, track_classes_data = load_curved_3d_data(data_path="hits_and_tracks_3d_3curved_events_all.csv", max_num_hits=540)
+max_nr_hits = 1200
+hits_data, track_params_data, track_classes_data = load_trackml_data(data_path="trackml_50to100tracks.csv", max_num_hits=max_nr_hits)
 dataset = HitsDataset(hits_data, track_params_data, track_classes_data)
 train_loader, valid_loader, test_loader = get_dataloaders(dataset,
                                                               train_frac=0.7,
                                                               valid_frac=0.15,
                                                               test_frac=0.15,
-                                                              batch_size=1024)
+                                                              batch_size=1)
 print('data loaded')
 
 preds, score = predict(transformer, test_loader)
-events = list(preds.keys())
-preds = list(preds.values())
 print(score)
-# plot_3d(preds, "theta")
-# plot_3d(preds, "phi")
-# plot_cluster_labels(preds, events)
-# plot_regressed_params(preds, events)
