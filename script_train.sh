c391a874c2e9d5f6ba7e5671b0ec837802113c42
nadiand <nadetodobreva@gmail.com>
2024-01-17 20:42:11 +0100

    Add loading cuda

--- script_train.sh
+++ script_train.sh
@@ -10,6 +10,8 @@ cd /projects/0/nisei0750/nadia/repo/hep_transformer/
 
 module purge
 module load 2022 Python/3.10.4-GCCcore-11.3.0
+module load CUDA/11.8.0
+module load cuDNN/8.6.0.163-CUDA-11.8.0
 
 pip install torch==2.1.0
 pip install scikit-learn
