574cb9b60128361a7312d0176f9893805dd8d759
nadiand <nadetodobreva@gmail.com>
2024-01-18 11:32:24 +0100

    Update script

--- script_train.sh
+++ script_train.sh
@@ -13,12 +13,16 @@ module load 2022 Python/3.10.4-GCCcore-11.3.0
 module load CUDA/11.8.0
 module load cuDNN/8.6.0.163-CUDA-11.8.0
 
-pip install torch==1.13.0
+pip install torch==2.1.0
 pip install scikit-learn
 pip install pandas
 
 pip install packaging
 pip install ninja
 pip install flash-attn==1.0.4 --no-build-isolation
+
+ls /lib | grep cuda
+ls /usr/lib | grep cuda
+ls /usr/local/lib | grep cuda
 
 python training_trackml.py
