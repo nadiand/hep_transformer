c12e1319cedddda103915b7d30d3b88756022192
nadiand <nadetodobreva@gmail.com>
2024-01-20 15:47:48 +0100

    Use new functions and files in test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch
 from model import TransformerClassifier
-from training import predict
+from training_f16 import predict
 from dataset import HitsDataset, get_dataloaders, load_linear_3d_data, load_linear_2d_data, load_curved_3d_data
 from plotting import *
 from trackml_data import load_trackml_data
@@ -15,13 +15,12 @@ transformer = TransformerClassifier(num_encoder_layers=6,
                                     dim_feedforward=128,
                                     dropout=0.1)
 transformer = transformer.to(DEVICE)
-checkpoint = torch.load("trackml_1to50tracks_3out_last", map_location=torch.device('cpu'))
+checkpoint = torch.load("test_best", map_location=torch.device('cpu'))
 transformer.load_state_dict(checkpoint['model_state_dict'])
 pytorch_total_params = sum(p.numel() for p in transformer.parameters() if p.requires_grad)
 print("Total trainable params: {}".format(pytorch_total_params))
 
-max_nr_hits = 1200
-hits_data, track_params_data, track_classes_data = load_trackml_data(data_path="trackml_50to100tracks.csv", max_num_hits=max_nr_hits)
+hits_data, track_params_data, track_classes_data = load_trackml_data(data_path="../../trackml_data_50tracks.csv")
 dataset = HitsDataset(hits_data, track_params_data, track_classes_data)
 train_loader, valid_loader, test_loader = get_dataloaders(dataset,
                                                               train_frac=0.7,
