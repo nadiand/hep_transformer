80247d00db77cbaa2d4bbcae43234ea5776b2965
nadiand <nadetodobreva@gmail.com>
2024-01-21 01:02:40 +0100

    Modify script

--- script_train.sh
+++ script_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train          # Job name
 #SBATCH --ntasks=1                    # Run on a single GPU
-#SBATCH --time=01:00:00               # Time limit hrs:min:sec
+#SBATCH --time=24:00:00               # Time limit hrs:min:sec
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
 #SBATCH --mail-type=BEGIN,END
@@ -17,4 +17,4 @@ pip install torch==2.1.0
 pip install scikit-learn
 pip install pandas
 
-python training_f16.py
+python training.py
