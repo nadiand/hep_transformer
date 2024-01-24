3ad8545dba94cfe0fade4f74b3075fdca6f68070
nadiand <nadetodobreva@gmail.com>
2024-01-24 09:17:46 +0100

    New type of loading data

--- training.py
+++ training.py
@@ -2,18 +2,19 @@ import torch
 import torch.nn as nn
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
-from hdbscan import HDBSCAN
-from torch.utils.data import TensorDataset, DataLoader
+import pandas as pd
 
 from model import TransformerClassifier, PAD_TOKEN, save_model
-from dataset import HitsDataset, get_dataloaders, load_linear_2d_data, load_linear_3d_data, load_curved_3d_data
+from dataset import HitsDataset
 from scoring import calc_score_trackml
+from trackml_data import load_trackml_data
+from plotting import *
+from torch.utils.data import DataLoader
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def clustering(pred_params):
-    # clustering_algorithm = HDBSCAN()
-    clustering_algorithm = AgglomerativeClustering(n_clusters=None, distance_threshold=1.15)
+    clustering_algorithm = AgglomerativeClustering(n_clusters=None, distance_threshold=0.1)
     cluster_labels = []
     for _, event_prediction in enumerate(pred_params):
         regressed_params = np.array(event_prediction.tolist())
@@ -32,18 +33,24 @@ def train_epoch(model, optim, train_loader, loss_fn, scaler):
     torch.set_grad_enabled(True)
     model.train()
     losses = 0.
-    for data in train_loader:
-        hits, track_params = data
+    for i, data in enumerate(train_loader):
+        _, hits, track_params, _ = data
         optim.zero_grad()
 
-        # Make prediction
+        # Move to CUDA
         hits = hits.to(DEVICE)
         track_params = track_params.to(DEVICE)
         padding_mask = (hits == PAD_TOKEN).all(dim=2)
+
+        hits = torch.unsqueeze(hits[~padding_mask], 0)
+        track_params = torch.unsqueeze(track_params[~padding_mask], 0)
+
+        # Make prediction
         with torch.cuda.amp.autocast():
             pred = model(hits, padding_mask)
-            # Calculate loss and use it to update weights
             loss = loss_fn(pred, track_params)
+        
+        # Update loss and scaler
         scaler.scale(loss).backward()
         scaler.step(optim)
         scaler.update()
@@ -60,21 +67,23 @@ def evaluate(model, validation_loader, loss_fn):
     model.eval()
     losses = 0.
     with torch.no_grad():
-        for data in valid_loader:
+        for i, data in enumerate(valid_loader):
             _, hits, track_params, _ = data
 
             # Make prediction
             hits = hits.to(DEVICE)
             track_params = track_params.to(DEVICE)
-            
             padding_mask = (hits == PAD_TOKEN).all(dim=2)
-            pred = model(hits, padding_mask)
-            track_params = track_params[:, :pred.shape[1] ,:] 
 
-            # Calculate loss and use it to update weights
-            loss = loss_fn(pred, track_params)
+            hits = torch.unsqueeze(hits[~padding_mask], 0)
+            track_params = torch.unsqueeze(track_params[~padding_mask], 0)
+            
+            with torch.cuda.amp.autocast():
+                pred = model(hits, padding_mask)
+                loss = loss_fn(pred, track_params)
+
             losses += loss.item()
-
+            
     return losses / len(validation_loader)
 
 def predict(model, test_loader):
@@ -87,52 +96,43 @@ def predict(model, test_loader):
     predictions = {}
     score = 0.
     for data in test_loader:
-        hits, track_params = data
+        event_id, hits, track_params, track_labels = data
 
         # Make prediction
         hits = hits.to(DEVICE)
         track_params = track_params.to(DEVICE)
-        
+        track_labels = track_labels.to(DEVICE)
         padding_mask = (hits == PAD_TOKEN).all(dim=2)
-        pred = model(hits, padding_mask)
-        print(pred)
-        track_params = track_params[:, :pred.shape[1] ,:]
-        track_labels = track_labels[:, :pred.shape[1]]
-        hits = hits[:, :pred.shape[1], :]
+
+        hits = torch.unsqueeze(hits[~padding_mask], 0)
+        track_params = torch.unsqueeze(track_params[~padding_mask], 0)
+        track_labels = torch.unsqueeze(track_labels[~padding_mask], 0)
+
+        with torch.cuda.amp.autocast():
+            pred = model(hits, padding_mask)
 
         cluster_labels = clustering(pred)
         event_score = calc_score_trackml(cluster_labels, track_labels)
         score += event_score
 
-        predictions = None
-        # for _, e_id in enumerate(event_id):
-        #     predictions[e_id.item()] = (hits, pred, track_params, cluster_labels, track_labels, event_score)
+        for _, e_id in enumerate(event_id):
+            predictions[e_id.item()] = (hits, pred, track_params, cluster_labels, track_labels, event_score)
 
     return predictions, score/len(test_loader)
 
 if __name__ == "__main__":
-    NUM_EPOCHS = 1
+    NUM_EPOCHS = 5
     EARLY_STOPPING = 50
-    MODEL_NAME = "test"
-    max_nr_hits = 800
+    MODEL_NAME = "flash"
+    hits_per_event = 620
+    CHUNK_SIZE = hits_per_event*100
 
     torch.manual_seed(37)  # for reproducibility
 
-    # Load and split dataset into training, validation and test sets, and get dataloaders
-    # hits_data, track_params_data, track_classes_data = load_curved_3d_data(data_path="hits_and_tracks_3d_3curved_events_all.csv", max_num_hits=max_nr_hits)
-    # dataset = HitsDataset(hits_data, track_params_data, track_classes_data)
-    # train_loader, valid_loader, test_loader = get_dataloaders(dataset,
-    #                                                           train_frac=0.7,
-    #                                                           valid_frac=0.15,
-    #                                                           test_frac=0.15,
-    #                                                           batch_size=2)
-    # print("data loaded")
-
-    my_x = torch.Tensor([np.array([[1.0,2.15, 0.56],[3.1,4.67, 1.5]]),np.array([[5.,6.12,2.36],[1.24,0.76,5.23]])])
-    my_y = torch.Tensor([np.array([2.]), np.array([2.])])
-
-    my_dataset = TensorDataset(my_x,my_y)
-    my_dataloader = DataLoader(my_dataset)
+    # Load validation data, and get a dataloader
+    hits_data, track_params_data, track_classes_data = load_trackml_data(data_path="trackml_1to5tracks.csv", normalize=True)
+    val_dataset = HitsDataset(hits_data, track_params_data, track_classes_data)
+    valid_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 
     # Transformer model
     transformer = TransformerClassifier(num_encoder_layers=6,
@@ -156,31 +156,35 @@ if __name__ == "__main__":
     count = 0
 
     for epoch in range(NUM_EPOCHS):
-        train_loss = train_epoch(transformer, optimizer, my_dataloader, loss_fn, scaler)
-        print(train_loss)
+        # Train the model
+        train_losses = []
+        with pd.read_csv("trackml_data_50tracks.csv", chunksize=CHUNK_SIZE) as reader:
+            for chunk in reader:
+                hits_data, track_params_data, track_classes_data = load_trackml_data(chunk)
+                train_dataset = HitsDataset(hits_data, track_params_data, track_classes_data)
+                train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
+                loss = train_epoch(transformer, optimizer, train_loader, loss_fn)
+                train_losses.append(loss)
+        train_loss = np.array(train_losses).mean()
 
-    # for epoch in range(NUM_EPOCHS):
-    #     # Train the model
-    #     train_loss = train_epoch(transformer, optimizer, train_loader, loss_fn)
-    #     val_loss = evaluate(transformer, valid_loader, loss_fn)
-    #     print(f"Epoch: {epoch}\nVal loss: {val_loss:.8f}, Train loss: {train_loss:.8f}")
+        # Evaluate using validation split
+        val_loss = evaluate(transformer, valid_loader, loss_fn)
+        
+        # Bookkeeping
+        print(f"Epoch: {epoch}\nVal loss: {val_loss:.8f}, Train loss: {train_loss:.8f}", flush=True)
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
 
-    #     train_losses.append(train_loss)
-    #     val_losses.append(val_loss)
+        if val_loss < min_val_loss:
+            # If the model has a new best validation loss, save it as "the best"
+            min_val_loss = val_loss
+            save_model(transformer, optimizer, "best", val_losses, train_losses, epoch, count, MODEL_NAME)
+            count = 0
+        else:
+            # If the model's validation loss isn't better than the best, save it as "the last"
+            save_model(transformer, optimizer, "last", val_losses, train_losses, epoch, count, MODEL_NAME)
+            count += 1
 
-    #     if val_loss < min_val_loss:
-    #         # If the model has a new best validation loss, save it as "the best"
-    #         min_val_loss = val_loss
-    #         save_model(transformer, optimizer, "best", val_losses, train_losses, epoch, count, MODEL_NAME)
-    #         count = 0
-    #     else:
-    #         # If the model's validation loss isn't better than the best, save it as "the last"
-    #         save_model(transformer, optimizer, "last", val_losses, train_losses, epoch, count, MODEL_NAME)
-    #         count += 1
-
-    #     if count >= EARLY_STOPPING:
-    #         print("Early stopping...")
-    #         break
-
-    preds, score = predict(transformer, my_dataloader)
-    print(score)
+        if count >= EARLY_STOPPING:
+            print("Early stopping...")
+            break
