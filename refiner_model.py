9be84adea345697c3d2c9ce7c8477cc119e77d4f
nadiand <nadetodobreva@gmail.com>
2024-03-15 09:42:06 +0100

    Uncomment sigmoid

--- refiner_model.py
+++ refiner_model.py
@@ -16,13 +16,13 @@ class RefinerTransformer(nn.Module):
         encoder_layers = nn.TransformerEncoderLayer(d_model, n_head, dim_feedforward, dropout, batch_first=True)
         self.encoder = nn.TransformerEncoder(encoder_layers, num_encoder_layers)
         self.decoder = nn.Linear(d_model, output_size)
-        # self.class_label = nn.Sigmoid()
+        self.class_label = nn.Sigmoid()
 
     def forward(self, input, padding_mask):
         x = self.input_layer(input)
         memory = self.encoder(src=x, src_key_padding_mask=padding_mask)
         out = self.decoder(memory)
-        return out #self.class_label(out)
+        return self.class_label(out)
 
 def save_model(model, optim, type, val_losses, train_losses, epoch, count, file_name):
     print(f"Saving {type} model")
