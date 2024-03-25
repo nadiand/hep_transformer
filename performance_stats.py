282d8ca2eb50f9051e786669b11e34cf3b96e77f
nadiand <nadetodobreva@gmail.com>
2024-03-25 17:45:19 +0100

    Implement cpu timer

--- performance_stats.py
+++ performance_stats.py
@@ -4,6 +4,8 @@ import torch.cuda as cuda
 import numpy as np
 import time
 
+from time import perf_counter_ns, process_time_ns
+
 from training import clustering
 from scoring import calc_score, calc_score_trackml
 from model import TransformerClassifier, PAD_TOKEN
@@ -92,21 +94,51 @@ def predict_with_cudatime(model, test_loader, min_cl_size, min_samples, include_
     print("Avg CUDA time:", sum(cuda_times[1:])/len(cuda_times[1:]))
     
 
+def predict_with_cputime(model, test_loader, min_cl_size, min_samples):
+    # Get the network in evaluation mode
+    torch.set_grad_enabled(False)
+    model.eval()
+
+    time_clocks = []
+    cpu_times = []
+
+    for data in test_loader:
+        _, hits, track_params, track_labels = data
+
+        padding_mask = (hits == PAD_TOKEN).all(dim=2)
+        pred = model(hits, padding_mask)
+
+        pred = torch.unsqueeze(pred[~padding_mask], 0)
+        track_params = torch.unsqueeze(track_params[~padding_mask], 0)
+        track_labels = torch.unsqueeze(track_labels[~padding_mask], 0)
+
+        start_timer = perf_counter_ns()
+        start_cpu_time = process_time_ns()
+        cluster_labels = clustering(pred, min_cl_size, min_samples)
+        end_timer = perf_counter_ns()
+        end_cpu_time = process_time_ns()
+
+        time_clocks.append(end_timer - start_timer)
+        cpu_times.append(end_cpu_time - start_cpu_time)
+
+    print(cpu_times, time_clocks)
+    print("Avg CPU time:", sum(cpu_times[1:])/len(cpu_times[1:]))
+
 transformer = TransformerClassifier(num_encoder_layers=6,
                                     d_model=32,
-                                    n_head=8,
-                                    input_size=2,
-                                    output_size=1,
+                                    n_head=4,
+                                    input_size=3,
+                                    output_size=4,
                                     dim_feedforward=128,
                                     dropout=0.1)
 transformer = transformer.to(DEVICE)
 
-checkpoint = torch.load("2d_model_best", map_location=torch.device('cpu'))
+checkpoint = torch.load("models/10to50_sin_cos_best", map_location=torch.device('cpu'))
 transformer.load_state_dict(checkpoint['model_state_dict'])
 pytorch_total_params = sum(p.numel() for p in transformer.parameters() if p.requires_grad)
 print("Total trainable params: {}".format(pytorch_total_params))
 
-hits_data, track_params_data, track_classes_data = load_linear_2d_data(data_path="hits_and_tracks_2d_events_all.csv") #, max_num_hits=100)
+hits_data, track_params_data, track_classes_data = load_trackml_data(data="trackml_10to50tracks_40kevents.csv", max_num_hits=700, normalize=True)
 dataset = HitsDataset(hits_data, track_params_data, track_classes_data)
 train_loader, valid_loader, test_loader = get_dataloaders(dataset,
                                                               train_frac=0.7,
@@ -115,9 +147,9 @@ train_loader, valid_loader, test_loader = get_dataloaders(dataset,
                                                               batch_size=1)
 print('data loaded')
 
-min_cl_size, min_samples = 5, 5
-preds = predict_with_cudatime(transformer, test_loader, min_cl_size, min_samples)
-preds = list(preds.values())
+min_cl_size, min_samples = 5, 2
+preds = predict_with_cputime(transformer, test_loader, min_cl_size, min_samples)
+# preds = list(preds.values())
 
 
 # for param in ['theta', 'phi', 'q']:
