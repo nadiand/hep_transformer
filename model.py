31864dbf84b660a21ec097a1eb2c1a2cafb57db0
nadiand <nadetodobreva@gmail.com>
2024-04-04 09:26:53 +0200

    Fix layer name

--- model.py
+++ model.py
@@ -31,12 +31,12 @@ class TransformerRegressor(nn.Module):
         else:
             encoder_layers = nn.TransformerEncoderLayer(d_model, n_head, dim_feedforward, dropout, batch_first=True)
         self.encoder = nn.TransformerEncoder(encoder_layers, num_encoder_layers, enable_nested_tensor=False)
-        self.output_layer = nn.Linear(d_model, output_size)
+        self.decoder = nn.Linear(d_model, output_size)
 
     def forward(self, input, padding_mask):
         x = self.input_layer(input)
         memory = self.encoder(src=x, src_key_padding_mask=padding_mask)
-        out = self.output_layer(memory)
+        out = self.decoder(memory)
         return out
 
 
