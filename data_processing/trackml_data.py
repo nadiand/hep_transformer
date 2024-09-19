1b43a96cf53a631ea7e9006d2f3df1bcd5fcdd37
nadiand <nadetodobreva@gmail.com>
2024-09-19 13:49:18 +0200

    Add data loading function with specified range for momentum p

--- data_processing/trackml_data.py
+++ data_processing/trackml_data.py
@@ -94,7 +94,7 @@ def load_trackml_data(data, max_num_hits, normalize=False, chunking=False):
     chunking allows for reading .csv files in chunks.
     """
     if not chunking:
-        data = pd.read_csv(data).head(10000)
+        data = pd.read_csv(data)
 
     # Normalize the data if applicable
     if normalize:
@@ -102,6 +102,65 @@ def load_trackml_data(data, max_num_hits, normalize=False, chunking=False):
             mean = data[col].mean()
             std = data[col].std()
             data[col] = (data[col] - mean)/std
+
+    # Shuffling the data and grouping by event ID
+    shuffled_data = data.sample(frac=1)
+    data_grouped_by_event = shuffled_data.groupby("event_id")
+
+    def extract_hits_data(event_rows):
+        # Returns the hit coordinates as a padded sequence; this is the input to the transformer
+        event_hit_data = event_rows[["x", "y", "z"]].to_numpy(dtype=np.float32)
+        return np.pad(event_hit_data, [(0, max_num_hits-len(event_rows)), (0, 0)], "constant", constant_values=PAD_TOKEN)
+
+    def extract_track_params_data(event_rows):
+        # Returns the track parameters as a padded sequence; this is what the transformer must regress
+        event_track_params_data = event_rows[["px","py","pz","q"]].to_numpy(dtype=np.float32)
+        p = np.sqrt(event_track_params_data[:,0]**2 + event_track_params_data[:,1]**2 + event_track_params_data[:,2]**2)
+        theta = np.arccos(event_track_params_data[:,2]/p)
+        phi = np.arctan2(event_track_params_data[:,1], event_track_params_data[:,0])
+        processed_event_track_params_data = np.column_stack([theta, np.sin(phi), np.cos(phi), event_track_params_data[:,3]])
+        return np.pad(processed_event_track_params_data, [(0, max_num_hits-len(event_rows)), (0, 0)], "constant", constant_values=PAD_TOKEN)
+
+    def extract_hit_classes_data(event_rows):
+        # Returns the particle information as a padded sequence; this is used for weighting in the calculation of trackML score
+        event_hit_classes_data = event_rows[["particle_id","weight"]].to_numpy(dtype=np.float32)
+        return np.pad(event_hit_classes_data, [(0, max_num_hits-len(event_rows)), (0, 0)], "constant", constant_values=PAD_TOKEN)
+
+    # Get the hits, track params and their weights as sequences padded up to a max length
+    grouped_hits_data = data_grouped_by_event.apply(extract_hits_data)
+    grouped_track_params_data = data_grouped_by_event.apply(extract_track_params_data)
+    grouped_hit_classes_data = data_grouped_by_event.apply(extract_hit_classes_data)
+
+    # Stack them together into one tensor
+    hits_data = torch.tensor(np.stack(grouped_hits_data.values))
+    track_params_data = torch.tensor(np.stack(grouped_track_params_data.values))
+    hit_classes_data = torch.tensor(np.stack(grouped_hit_classes_data.values))
+
+    return hits_data, track_params_data, hit_classes_data
+
+
+def load_trackml_data_pt(data, max_num_hits, pt_min, pt_max, normalize=False, chunking=False):
+    """
+    Function for reading .csv file with TrackML data and creating tensors
+    containing the hits and ground truth information from it.
+    max_num_hits denotes the size of the largest event, to pad the other events
+    up to. normalize decides whether the data will be normalized first. 
+    chunking allows for reading .csv files in chunks.
+    pt_min and pt_max determine the range of the momentum p that the hits must
+    fall into to be part of the dataset.
+    """
+    if not chunking:
+        data = pd.read_csv(data)
+
+    # Normalize the data if applicable
+    if normalize:
+        for col in ["x", "y", "z", "px", "py", "pz", "q"]:
+            mean = data[col].mean()
+            std = data[col].std()
+            data[col] = (data[col] - mean)/std
+
+    data['p'] = np.sqrt(data["px"]**2 + data["py"]**2 + data["pz"]**2)
+    data = data[np.logical_and(data['p'] >= pt_min, data['p'] < pt_max)]
 
     # Shuffling the data and grouping by event ID
     shuffled_data = data.sample(frac=1)
