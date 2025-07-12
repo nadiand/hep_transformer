2575f2827c9d4b4a7c76a789bbef53843126346f
nadiand <nadetodobreva@gmail.com>
2025-07-12 10:51:37 +0200

    Update bash script

--- script_train.sh
+++ script_train.sh
@@ -2,19 +2,30 @@
 #SBATCH --job-name=train              # Job name
 #SBATCH --ntasks=1                    # Run on a single GPU
 #SBATCH --time=24:00:00               # Time limit hrs:min:sec
-#SBATCH --partition=gpu
+#SBATCH --partition=gpu_a100
 #SBATCH --gpus-per-node=1
 #SBATCH --mail-type=BEGIN,END
 
 cd /projects/0/nisei0750/nadia/repo/hep_transformer/
 
 module purge
-module load 2022 Python/3.10.4-GCCcore-11.3.0
-module load CUDA/11.8.0
-module load cuDNN/8.6.0.163-CUDA-11.8.0
+module load 2023
+module load CUDA/12.1.1
 
-pip install torch==2.1.2
+python -m venv trackformers
+source trackformers/bin/activate
+
+pip install torch==2.7
 pip install scikit-learn
 pip install pandas
+pip install matplotlib
 
-python training.py --nr_epochs 10 --max_nr_hits 100 --data_path "hits_and_tracks_3d_3curved_events_all.csv" --model_name "3tracks_helical" --nr_enc_layers 2 --embedding_size 64 --hidden_dim 128 --data_type "curved"
+pip install mambapy
+pip install lightning_utilities
+pip install hdbscan
+
+nvcc --version
+#pip freeze > requirements.txt
+
+python training_flash.py
+#python test.py
